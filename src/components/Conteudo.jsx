c6586f52b3aee97ce498845270ee423aa1b82d02
victor <124385408+77bueno@users.noreply.github.com>
2023-12-05 11:36:11 -0300

    Refatoração

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,12 +1,6 @@
 /* Conteudo.jsx */
 import Artigo from "./Artigo";
 import styled from "styled-components";
-
-/* Importando os assets de imagem */
-import imagem1 from "../assets/abra-o-livro.png";
-import imagem2 from "../assets/livro-magico.png";
-import imagem3 from "../assets/pilha-de-livros.png";
-
 import cursos from "../api/cursos.jsx";
 
 const StyledConteudo = styled.main`
