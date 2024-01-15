6e066192430101d863c2319a08a506917f294871
victor <victorhugomotabueno@gmail.com>
2024-01-15 12:00:26 -0300

    Loading

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from "react";
 
 function Produtos(){
-    /* O state "produtos" é iniciado como um array vazio.
-    Posteriormente (após o carregamento dos dados da API),
-    ele será preenchido com os objetos/produtos. */
-    const [produtos, SetProdutos] = useState([]);
+     /* O state "produtos" é iniciado como um array vazio.
+  Posteriormente (após o carregamento dos dados da API),
+  ele será preenchido com os objetos/produtos. */
+  const [produtos, setProdutos] = useState([]);
 
-    /* Gerenciamento o efeito colateral 
-    do componente para o carregamento dos dados da API. */
+  // State de loading (por padrão, inicia ativado/true)
+  const [loading, setLoading] = useState(true);
+
+  /* Gerenciando o efeito colateral
+  do componente para o carregamento dos dados da API. 
+  
+  Fluxo geral de funcionamento do código abaixo:
+
+  1) useEffect é carregado UMA VEZ e APÓS a montagem da page (Produtos)
+  Obs.: o [] indica que o useEffect não tem dependências adicionais
+  e que será executado somente UMA VEZ.
+
+  2) Em seguida, ele executa a função carregarDados
+  3) Ao término dela, atualiza o state (produtos)  */
     useEffect( () => {
         const carregarDados = async () => {
             try {
                 const resposta = await fetch(`https://fakestoreapi.com/products`);
                 const dados = await resposta.json();
                 console.log(dados);
-                SetProdutos(dados);
+                setProdutos(dados); // adicionamos ao state
+                setLoading(false); // desativamos o loading
             } catch (error) {
                 console.error("Houve um erro: "+error);
             }
@@ -27,13 +40,15 @@ function Produtos(){
         <article>
             <h2>Produtos</h2>
 
-            {produtos.map( produto => { 
-                return <section key={produto.id}> 
-                    <h3> {produto.title} </h3>
-                    <p>Preço: {produto.price} </p>
-                    <p> {produto.description} </p>
-                </section>
-            })}
+            { loading ? <p>Carregando...</p> : (
+                produtos.map( produto => { 
+                    return <section key={produto.id}> 
+                        <h3> {produto.title} </h3>
+                        <p>Preço: {produto.price} </p>
+                        <p> {produto.description} </p>
+                    </section>
+                })
+            ) }
         </article>
     );
 }
