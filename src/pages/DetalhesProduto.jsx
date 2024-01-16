c1a6520b848a4d7431262c6b7448a2e6e6f1941c
victor <victorhugomotabueno@gmail.com>
2024-01-16 09:18:55 -0300

    useParams

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 function DetalhesProduto(){
+    const { id } = useParams();
+
     return <article>
         <h2>Titulo...</h2>
         <p><b>Categoria: </b> Categoria...</p>
