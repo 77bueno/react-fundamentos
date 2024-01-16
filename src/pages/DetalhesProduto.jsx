79081f75b5799db15d0c72f0920cb95d7b45c25d
victor <victorhugomotabueno@gmail.com>
2024-01-16 09:34:14 -0300

    Carregamento dos dados da API

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function DetalhesProduto(){
+    /* Usamos o hook useParams do React Router DOM
+    para ter acesso aos parâmetros da rota dinâmica
+    neste caso, o parâmetro chamado "id". */
     const { id } = useParams();
+
+    useEffect( () => {
+        const carregarDados = async () => {
+            try {
+                const resposta = await fetch(`https://fakestoreapi.com/products/${id}`);
+                const dados = await resposta.json()
+            } catch (error) {
+                console.error("Erro ao carregar produto: "+error);
+            }
+        }
+    }, [id] );
 
     return <article>
         <h2>Titulo...</h2>
